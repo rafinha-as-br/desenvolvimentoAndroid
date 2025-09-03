1f703648629d971b8d2f4fafb9cbf739c38966e7
Rafael Antunes <rafinha72465@gmail.com>
2025-09-03 20:04:04 -0300

    sorteio work

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,24 +1,58 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity2 extends AppCompatActivity {
+    Button button;
+    EditText editTextMin, editTextMax;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        button = findViewById(R.id.button);
+        editTextMin = findViewById(R.id.edMin);
+        editTextMax = findViewById(R.id.edMax);
+        tv= findViewById(R.id.tvResultado);
+
+        button.setOnClickListener(v-> {
+            Random random = new Random();
+            int min, max;
+            min = Integer.parseInt(editTextMin.getText().toString() );
+            max = Integer.parseInt(editTextMax.getText().toString());
+            int delta = max-min;
+            int sorteado = random.nextInt(delta)+min;
+
+            tv.setText(Integer.toString(sorteado));
+
         });
+
+
+
     }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putString("sorteado", tv.getText().toString());
+    }
+
+
+
 }
