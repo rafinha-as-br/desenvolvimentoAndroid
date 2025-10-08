65fcbc2911c4871d7cd2a11c7083e3f2cedde1e0
rafinha <rafinha72465@gmail.com>
2025-10-08 19:39:00 -0300

    fiz um app que dá para adicionar textos a uma lista

--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -2,6 +2,11 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,33 +14,53 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity3 extends AppCompatActivity {
+
+
+    ListView listView;
+    ArrayList<String> nomes;
+
+    EditText editText;
+
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main3);
-        Log.d("ciclo_vida", "onCreate");
+
+        nomes = new ArrayList<>(){{
+            add("Apple");
+            add("Banana");
+            add("Cherry");
+        }};
+
+        editText = findViewById(R.id.textField);
+
+
+
+
+        listView = findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>( this,
+                android.R.layout.simple_list_item_1,
+                nomes
+        );
+
+        button = findViewById(R.id.button);
+        listView.setAdapter(adapter);
+
+        button.setOnClickListener(click -> {
+
+            nomes.add(editText.getText().toString());
+            listView.setAdapter(adapter);
+
+        });
+
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("ciclo_vida", "onStart");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("ciclo_vida", "onRestart");
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("ciclo_vida", "onDestroy");
-
-    }
 }
