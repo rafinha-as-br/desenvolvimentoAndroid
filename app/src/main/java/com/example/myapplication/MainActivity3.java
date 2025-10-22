86387a447b93ef67b6349989049a5565f660e1e5
rafinha <rafinha72465@gmail.com>
2025-10-22 19:17:39 -0300

    work on app list

--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
@@ -29,7 +31,15 @@ public class MainActivity3 extends AppCompatActivity {
 
         pm=getPackageManager();
 
-        List<ApplicationInfo> apps = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        ArrayList<ApplicationInfo> apps = new ArrayList<>();
+        Intent iquery = new Intent(Intent.ACTION_MAIN, null);
+        iquery.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> listResolvInfo = pm.queryIntentActivities(iquery, PackageManager.GET_META_DATA);
+
+        for(ResolveInfo resolveInfo : listResolvInfo){
+            apps.add(resolveInfo.activityInfo.applicationInfo);
+        }
 
         AppAdapter adapter = new AppAdapter(this, R.layout.app, apps);
         listView.setAdapter(adapter);
