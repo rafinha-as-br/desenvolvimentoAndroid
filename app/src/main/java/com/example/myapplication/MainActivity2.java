b98ee679a1359eb7f3d834bb4e25236322c60cf8
rafinha <rafinha72465@gmail.com>
2025-11-19 21:47:53 -0300

    gps funcionando, só precisa adicionar o mapa visualmente

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,62 +1,87 @@
 package com.example.myapplication;
-
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import java.util.List;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
-public class MainActivity2 extends AppCompatActivity implements SensorEventListener {
+public class MainActivity2 extends AppCompatActivity{
 
-    private SQLiteDatabase banco;
-    SensorManager sensorManager;
-    Sensor sensorLuz;
+    LocationManager locationManager;
+    TextView textView;
 
+    MapView mapView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main4);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mapView = findViewById(R.id.mapView);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(v-> getLocation());
+    }
 
-        banco = this.openOrCreateDatabase("banco", getBaseContext().MODE_PRIVATE, null);
+    @SuppressLint("SetTextI18n")
+    public void getLocation(){
 
-        sensorManager=(SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensorLuz= sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermission();
+            return;
+        }
+        //Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0, location1 -> {
+            double latitude = location1.getLatitude();
+            double longitude = location1.getLongitude();
+            textView.setText("longitude: " + latitude + " Longitude: " + longitude);
+            showLocationOnMap(latitude, longitude);
+        });
 
-        sensorManager.registerListener(this, sensorLuz,SensorManager.SENSOR_DELAY_NORMAL);
+//        if(location != null){
+//            double latitude = location.getLatitude();
+//            double longitude = location.getLongitude();
+//            textView.setText("longitude: " + latitude + " Longitude: " + longitude);
+//        } else{
+//            textView.setText(" A");
+//        }
+
+    }
+
+    public void showLocationOnMap(double latitude, double longitude){
+        GeoPoint userLocation = new GeoPoint(latitude, longitude);
+        mapView.getController().setCenter(userLocation);
+        mapView.getController().setZoom(15.0);
+        mapView.invalidate();
 
 
     }
 
-    public void inserirTabela(String nomeTabela, ContentValues values){
-        this.banco.insert(nomeTabela, null, values);
-    }
-
-    public void editarTabela(String nomeTabela, String[] id, ContentValues values){
-        this.banco.update(nomeTabela, values, "WHERE ID = ?", id);
-    }
-
-    public void remover(String nomeTabela, String[] id){
-        this.banco.delete(nomeTabela, "WHERE ID = ?", id);
+    public void requestPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1
+                    );
+        }
     }
 
 
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
 
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
-    }
 }
